bd0a1d45bb45a19f8482f6b62678c6c1fe1a12fe
Jessica Gomes <jessicaaraujogomes2015@gmail.com>
2022-08-30 16:25:01 -0300

    gerador de senha automatico

---
+++ script.js
@@ -0,0 +1,43 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button")
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+
+let containerPassword = document.querySelector("#container-password");
+
+
+
+let charset = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+
+slider.oninput = function(){
+    sizePassword.innerHTML= this.value;
+}
+
+
+function generatePassword(){
+
+    let pass = "";
+
+    for(let i =  0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() *n));
+
+    
+}
+containerPassword.classList.remove('hide');
+ password.innerHTML= pass;
+ novaSenha=pass;
+
+
+
+}
+
+function copyPassword(){
+    alert('senha copiada com sucesso!')
+    navigator.clipboard.writeText(novaSenha);
+}
