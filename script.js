5ca80019c2e676e8192f10a2ef2c30230bb1974c
Jessica Gomes <jessicaaraujogomes2015@gmail.com>
2022-08-30 17:22:32 -0300

    geradordesenha

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button")
+let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
